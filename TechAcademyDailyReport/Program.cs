1fb384bfbe92c8af49b62fb167ab5363c8bfe4a4
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-15 19:48:34 -0400

    built daily report

--- TechAcademyDailyReport/Program.cs
+++ TechAcademyDailyReport/Program.cs
@@ -10,6 +10,26 @@ namespace TechAcademyDailyReport
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string courseType = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string pageNum = Console.ReadLine();
+            int numOfPage = Convert.ToInt32(pageNum);
+            Console.WriteLine("Do you need help with anything? Please answer \"True\" or \"False\"");
+            string needHelp = Console.ReadLine();
+            string helpStatus = Convert.ToString(needHelp);
+            Console.WriteLine("Are there any positive experiences you'd like to provide? Please be specific.");
+            string positiveExp = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudied = Console.ReadLine();
+            int studyingHours = Convert.ToInt32(hoursStudied);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.Read();
+            
         }
     }
 }
